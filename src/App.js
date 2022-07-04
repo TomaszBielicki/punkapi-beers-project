0dfcdcb52ee432258de7f5a427c48f919d815776
Tomek <tomek.bielicki@gmail.com>
2022-07-04 13:25:50 +0200

    Api connect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Wrapper } from "./components/Wrapper/Wrapper.styled";
+import { Header } from "./components/Header/Header.styled";
+import BeerList from "./components/BeerList/BeerList";
+import CustomButton from "./components/CustomButton/CustomButton";
 
 function App() {
+  const [beerList, setBeerList] = useState([]);
+  const [showFavBeers, setShowFavBeers] = useState(false);
+  const [currentPage, setCurrentPage] = useState(2);
+  const [beerPerPage, setBeerPerPage] = useState(2);
+
+  useEffect(() => {
+    axios
+      .get(`https://api.punkapi.com/v2/beers/?page=1&per_page=${beerPerPage}`)
+      .then((res) => {
+        setBeerList(res.data);
+        console.log("EFFECT DONE");
+      })
+
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
+  console.log("beer list: ", beerList);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Wrapper>
+      <Header>
+        <CustomButton onClick={() => setShowFavBeers(false)}>
+          All Beers
+        </CustomButton>
+        <CustomButton
+          onClick={() => {
+            setShowFavBeers(true);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Favourite Beers
+        </CustomButton>
+      </Header>
+      <BeerList
+        beerList={beerList}
+        showFavBeers={showFavBeers}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        setBeerList={setBeerList}
+        beerPerPage={beerPerPage}
+      />
+    </Wrapper>
   );
 }
 
